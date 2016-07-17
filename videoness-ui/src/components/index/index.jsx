c24175b7d2c8cb50ff0bf2d93ceb660f268b5038
Adi Kancherla <akancherla@apigee.com>
2016-07-16 22:12:54 -0700

    bootstrapped

--- videoness-ui/src/components/index/index.jsx
+++ videoness-ui/src/components/index/index.jsx
@@ -6,18 +6,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Video from 'react-html5video';
 
-require('./index.css');
 require('react-html5video/dist/ReactHtml5Video.css');
+require('./index.css');
+
+var Header = React.createClass({
+  render() {
+    return (
+      <nav className="navbar navbar-fixed-top vid-sticky-header">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand vid-title" href="#">welcome to videoness</a>
+          </div>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav navbar-right">
+              <li><a href="#" className="vid-sign-up-login"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+              <li><a href="#" className="vid-sign-up-login"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+});
 
 var Remlife = React.createClass({
   render() {
     return (
       <div>
-        <h2>to vividly remember life</h2>
+        <p className="vid-subtitle">to vividly remember life</p>
         <p>
           what were you doing three years ago on this day? right, nobody remembers. but what if you could?
           videoness helps you record your life and makes it searchable. never forget what happened on July 22 2004.
         </p>
+        <br/>
       </div>
     );
   }
@@ -25,30 +51,32 @@ var Remlife = React.createClass({
 
 var Ask = React.createClass({
   /*setVolume() {
-    this.refs.video.setVolume(0.5);
-  },*/
+   this.refs.video.setVolume(0.5);
+   },*/
   render() {
     var coolPlace = "<cool place>";
     return (
       <div>
-        <h2>to see what everyone is upto</h2>
+        <p className="vid-subtitle">to see what everyone is upto</p>
         <p>
           bored? curious? need info? see what friends are doing, see what is going on at {coolPlace},
           ask friends to do something that you want to see. like this:
         </p>
         <div className="row">
           <div className="col-md-5">
-            <Video ref="video" onLoadStart={this.setVolume} controls loop width="400" height="400">
-              <source src="https://firebasestorage.googleapis.com/v0/b/videoness-68f59.appspot.com/o/scottySire.mp4?alt=media&token=3577172d-15d5-463d-9a62-bd3f76c3e9c1"/>
+            <Video ref="video" onLoadStart={this.setVolume} controls loop>
+              <source
+                src="https://firebasestorage.googleapis.com/v0/b/videoness-68f59.appspot.com/o/scottySire.mp4?alt=media&token=3577172d-15d5-463d-9a62-bd3f76c3e9c1"/>
             </Video>
           </div>
-          <p className="btwText col-md-2">and videoness turns it into awesomeness:</p>
+          <p className="vid-btwText col-md-2">and videoness turns it into awesomeness:</p>
           <div className="jon col-md-5">
             ----Jon Seaton's eyes only----<br/> <br/>
             Jon, the "artified" video should look like this:
             <a target="_blank" href="https://vimeo.com/2810744">vincent van gogh effect</a>
           </div>
         </div>
+        <br/>
       </div>
     );
   }
@@ -58,9 +86,10 @@ var Selfexp = React.createClass({
   render() {
     return (
       <div>
-        <h2>to express better</h2>
+        <p className="vid-subtitle">to express better</p>
         <p>
-          i don't like typing (can't even touch type), text is a poor form of self expression. i want my status updates and reactions to
+          i don't like typing (can't even touch type), text is a poor form of self expression. i want my status updates
+          and reactions to
           be rich and fun. i want everything in video. i want a social profile of a new kind - one that has only video.
         </p>
       </div>
@@ -72,11 +101,13 @@ var Main = React.createClass({
   render() {
     return (
       <div>
-        <h1>why did i build videoness?</h1>
+        <Header/>
+        <p className="vid-main-title">why did i build videoness?</p>
         <Remlife/>
         <Ask/>
         <Selfexp/>
       </div>
+
     );
   }
 });
@@ -85,6 +116,6 @@ module.exports = Main;
 
 ReactDOM.render(
   <Main/>,
-  document.getElementById('main')
+  document.getElementById('container')
 );
 
