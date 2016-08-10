25cc1024e208bf58eb7f3466a87b0f1cdb1a5a1d
Adi Kancherla <akancherla@apigee.com>
2016-08-10 12:52:50 -0700

    added friends page

--- videoness-ui/src/components/friends/friends.js
+++ videoness-ui/src/components/friends/friends.js
@@ -3,12 +3,47 @@ import Header from '../common/header';
 
 require('./friends.css');
 
+var Friend = React.createClass({
+  render() {
+    return (
+      <div onClick={this.props.showFriendsTimeline} className="vid-friend-box row">
+        <div className="vid-friend-img-div col-xs-4">
+          <img className="vid-friend-img" src="/img/profile.jpg"/>
+        </div>
+        <div className="col-xs-8">
+          <p>name</p>
+          <p>other stuff</p>
+        </div>
+      </div>
+    );
+  }
+});
+
 var Friends = React.createClass({
+  showFriendsTimeline(obj) {
+    alert(obj);
+  },
   render() {
     return (
       <div>
         <Header/>
-        friends
+        <div className="vid-all-friends-box row">
+          <div className="col-md-4">
+            <Friend showFriendsTimeline={this.showFriendsTimeline.bind(this,"yoyo")}/>
+            <Friend/>
+            <Friend/>
+          </div>
+          <div className="col-md-4">
+            <Friend/>
+            <Friend/>
+            <Friend/>
+          </div>
+          <div className="col-md-4">
+            <Friend/>
+            <Friend/>
+            <Friend/>
+          </div>
+        </div>
       </div>
     );
   }
